e0a1a5279652e61b1775e7ae09db4cad9586ce71
Quynh Trang <quynhtranglengoc@gmail.com>
2025-07-03 18:42:03 +0700

    test public webpages

--- bleToWebApp/src/logic/beaconMap.ts
+++ bleToWebApp/src/logic/beaconMap.ts
@@ -3,8 +3,10 @@
  */
 
 export const beaconToUrl: Record<string, string> = {
-  "TraKieu_Apsara_Relief": "https://mercury.swin.edu.au/cos10025/project-group1/TraKieu_Apsara_Relief.html",
-  "Tara_Bodhisattva_Statue": "https://mercury.swin.edu.au/cos10025/project-group1/Tara_Bodhisattva_Statue.html",
+  // "TraKieu_Apsara_Relief": "https://mercury.swin.edu.au/cos10025/project-group1/TraKieu_Apsara_Relief.html",
+  // "Tara_Bodhisattva_Statue": "https://mercury.swin.edu.au/cos10025/project-group1/Tara_Bodhisattva_Statue.html",
+  "TraKieu_Apsara_Relief": "https://google.com",
+  "Tara_Bodhisattva_Statue": "https://youtube.com",
   "Cham_Dancing_Shiva": "https://mercury.swin.edu.au/cos10025/project-group1/Cham_Dancing_Shiva.html",
   "Ancient_Cham_Tower": "https://mercury.swin.edu.au/cos10025/project-group1/Ancient_Cham_Tower.html"
 };
